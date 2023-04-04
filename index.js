a3972315cd543fcb712e67eee43d56e6f0cdb7f8
John Metzger <jsmetzger9@gmail.com>
2023-04-04 18:43:39 -0400

    v2 push update w/ classes

--- index.js
+++ index.js
@@ -1,11 +1,16 @@
 const fs = require(`fs`);
 const inquirer = require(`inquirer`);
-
 const questions = [{
     type:'input',
     name:'logoName',
     message: 'Enter logo initials (3 characters maximum)',
-    maxLength:3
+    validate: (input)=> {
+        if (input.length > 3) {
+            return 'Logo name must not exceed 3 characters';
+
+        }
+        return true;
+    },
 },
 {
     type: 'input',
@@ -26,28 +31,49 @@ const questions = [{
     name:'shapeColor',
     message:'Choose a shape color'
 }];
+
+
+
 function GenerateSVG(data) {
-let shapeParam = data.shape;
-    switch(shapeParam){
+    class Circle {
+        constructor(params,textCentering){
+            this.params = `<circle cx="50" cy="50" r="40" fill="${data.shapeColor}" />`;
+            this.textCentering = `x="50" y="60"`;
+        }
+    }
+    class Triangle{
+        constructor(params,textCentering){
+            this.params = `<polygon points="50 15, 100 100, 0 100" fill="${data.shapeColor}" />`;
+            this.textCentering = `x="50" y="80"`;
+        }
+    }
+    class Square {
+        constructor(params,textCentering){
+            this.params = `<rect width ="100" height="100" fill="${data.shapeColor}" />`;
+            this.textCentering =`x="50" y="60"`;
+    }
+    }
+
+var shape = data.shape;
+
+    switch(shape){
         case 'Circle':
-            shapeParam = `<circle cx="50" cy="50" r="40" fill="${data.shapeColor}" />`;
+            var drawnShape = new Circle;
             break;
         case 'Triangle':
-            shapeParam = `<polygon points="50 15, 100 100, 0 100" fill="${data.shapeColor}" />`;
+            var drawnShape = new Triangle;
             break;
         case 'Square':
-            shapeParam = `<rect width ="100" height="100" fill="${data.shapeColor}" />`;
+            var drawnShape = new Square;
             break;
     }
 
     return `<?xml version="1.0" encoding="UTF-8" standalone="no"?>
 <!DOCTYPE svg PUBLIC "-//W3C//DTD SVG 1.1//EN" "http://www.w3.org/Graphics/SVG/1.1/DTD/svg11.dtd">
-<svg width="391" height="391" viewBox="-70.5 -70.5 391 391" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
-<rect fill="#fff" stroke="#000" x="-70" y="-70" width="390" height="390"/>
-<g opacity="0.8">
-    ${shapeParam}
-    <text x="32" y="50" text-anchor="middle" fill="white" font-size="30">Ayo?</text>
-</g>
+<svg width="300" height="200" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
+
+${drawnShape.params}
+<text ${drawnShape.textCentering} text-anchor="middle" fill="${data.textColor}" font-size="30">${data.logoName}</text>
 </svg>`;
 }
 
